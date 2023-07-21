a14220b6d842674d28c2e4668bb9f74f7331fb24
petchi <mpetchiyammal656@gmail.com>
2023-07-21 14:19:50 +0530

    test

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Students s1 = new Students();
+        s1.name="petchi";
+        s1.dept="ECE";
+        Students s2 = new Students();
+        s2.name="pavi";
+        s2.dept="EEE";
+
+        System.out.println(s1.name + " " + s1.dept);
+        System.out.println(s2.name + " " + s2.dept);
+    }
+}
+
+
---
+++ src/ex1.java
@@ -0,0 +1,14 @@
+import java util Scanner;
+public class ex1
+{
+    public static void main(string[]arg){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the 1st num");
+        Float num1=sc.nextFloat();
+        System.out.println("Enter the 2nd num");
+        Float num2=sc.nextFloat();
+        System.out.print(num1*num2);
+
+
+    }
+}
